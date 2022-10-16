9418fdaeba415711708394da9fa648617cfbff98
董叔叔 <610175956@qq.com>
2022-10-16 08:20:35 +0000

    Snake2.0
    Signed-off-by: 董叔叔 <610175956@qq.com>

---
+++ .gitee/Sanke/Snake2.0/SnakeGame/SnakeGame.h
@@ -0,0 +1,14 @@
+﻿// SnakeGame.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+#include <iostream>
+#include <thread>
+#include <windows.h>
+#include <conio.h>
+#include <time.h>
+#include <vector>
+#include <stdlib.h>
+
+// TODO: 在此处引用程序需要的其他标头。
